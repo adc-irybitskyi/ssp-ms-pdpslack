62f97b72f9e546846cc68ee0874133b553a20c0f
irybitskyi.ctr <irybitskyi.ctr@ten-x.com>
2016-07-20 13:35:09 -0700

    Add channel field to response

--- src/main/java/pdp/api/rest/ActionController.java
+++ src/main/java/pdp/api/rest/ActionController.java
@@ -1,7 +1,9 @@
-package pdp.api.rest.dto;
+package pdp.api.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pdp.api.rest.dto.ActionResponse;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +24,9 @@ class ActionController {
 
 	private Map<String, Boolean> approvedMap = new HashMap<>();
 
+	@Autowired
+	private ObjectMapper mapper;
+
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
@@ -27,11 +34,39 @@ class ActionController {
 	}
 
 	//https://api.slack.com/apps/A1T3W5N3T/interactive-messages
+
+/*
+  "actions": [
+    {
+      "name": "approve",
+      "value": "approve"
+    }
+  ],
+  "callback_id": "pof",
+  "team": {
+    "id": "T1Q7NUZUM",
+    "domain": "pdpslack"
+  },
+  "channel": {
+    "id": "C1QJB8QMS",
+    "name": "11"
+  },
+
+	 */
+
+
 	@RequestMapping(value = {"/pof"},  method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE )
 	@ResponseBody
 	String pofAction(@RequestBody final MultiValueMap<String, String > formVars) {
 		String payload = formVars.getFirst("payload");
 		LOGGER.info("pofAction payload:" + payload);
+		ActionResponse response;
+		try {
+			response = mapper.readValue(payload, ActionResponse.class);
+		} catch (IOException e) {
+			throw new RuntimeException("json formatter", e);
+		}
+		LOGGER.info("pofAction response:" + response);
 		//TODO: send message to PDP Chat with message 'Your Proof Of Funds were approved. Now you’re ready to make an offer.'
 		return "Buyer's Proof Of Funds is approved. Now he's ready to make an offer.";
 	}
--- src/main/java/pdp/api/rest/dto/ActionResponse.java
+++ src/main/java/pdp/api/rest/dto/ActionResponse.java
@@ -1,4 +1,19 @@
 package pdp.api.rest.dto;
 
 public class ActionResponse {
+	private Channel channel;
+
+	public Channel getChannel() {
+		return channel;
+	}
+
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+	}
+
+	@Override public String toString() {
+		return "ActionResponse{" +
+				"channel=" + channel +
+				'}';
+	}
 }
