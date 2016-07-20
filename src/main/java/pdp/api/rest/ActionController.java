74535d6ae534ba58f59c14758aefa08dbe84983c
irybitskyi.ctr <irybitskyi.ctr@ten-x.com>
2016-07-20 13:55:43 -0700

    show different response in case approved\decline

--- src/main/java/pdp/api/rest/ActionController.java
+++ src/main/java/pdp/api/rest/ActionController.java
@@ -16,6 +16,7 @@ import pdp.api.rest.dto.ActionResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Controller
 @RequestMapping("/action")
@@ -46,9 +47,16 @@ class ActionController {
 			throw new RuntimeException("json formatter", e);
 		}
 		LOGGER.info("pofAction response:" + response);
-		//TODO: send message to PDP Chat with message 'Your Proof Of Funds were approved. Now you’re ready to make an offer.'
-		return "Buyer's Proof Of Funds is approved. Now he's ready to make an offer.";
+		if (isApproved.apply(response)) {
+			//TODO: send message to PDP Chat with message 'Your Proof Of Funds were approved. Now you’re ready to make an offer.'
+			return "Buyer's Proof Of Funds is approved. Now he's ready to make an offer.";
+		} else {
+			//TODO: send message to PDP Chat with message 'Your Proof Of Funds were declined. Please provide new Proof of Funds document(s).'
+			return "Proof Of Funds is declined. Buyer should provide new Proof of Funds documents.";
+		}
 	}
+
+	private Function<ActionResponse, Boolean> isApproved = x -> x.getActions().stream().filter(a -> "approve".equals(a.getValue())).findAny().isPresent();
 
 	public static void main(String[] args) throws Exception {
 		String payload = "{\"actions\":[{\"name\":\"approve\",\"value\":\"approve\"}],\"callback_id\":\"pof\",\"team\":{\"id\":\"T1Q7NUZUM\",\"domain\":\"pdpslack\"},\"channel\":{\"id\":\"C1QJB8QMS\",\"name\":\"11\"},\"user\":{\"id\":\"U1Q8005KL\",\"name\":\"yury\"},\"action_ts\":\"1469046981.756851\",\"message_ts\":\"1469046975.000004\",\"attachment_id\":\"1\",\"token\":\"hPW7iJhJ96foXiYINFoMIs6Z\",\"original_message\":{\"type\":\"message\",\"user\":\"U1Q8005KL\",\"text\":\"Buyer uploaded Proof Of Funds Document with comments 'Send you one billion saving account statement'\",\"bot_id\":\"B1TG6L4U8\",\"attachments\":[{\"callback_id\":\"pof\",\"fallback\":\"You are unable to approve Proof Of Funds\",\"text\":\"Please approve Proof Of Funds Document <http:\\/\\/buyer1-pof.box.com>\",\"id\":1,\"actions\":[{\"id\":\"1\",\"name\":\"approve\",\"text\":\"Approve\",\"type\":\"button\",\"value\":\"approve\",\"style\":\"\"},{\"id\":\"2\",\"name\":\"decline\",\"text\":\"Decline\",\"type\":\"button\",\"value\":\"decline\",\"style\":\"\",\"confirm\":{\"text\":\"Provided Proof Of Funds Documents are invalid?\",\"title\":\"Are you sure?\",\"ok_text\":\"Yes\",\"dismiss_text\":\"No\"}}]}],\"ts\":\"1469046975.000004\"},\"response_url\":\"https:\\/\\/hooks.slack.com\\/actions\\/T1Q7NUZUM\\/61668710642\\/Uoaur3cKibnKffezoDw8oxvk\"}";
