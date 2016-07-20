cc834590f02a833ed4c7495d595836a82c0db207
irybitskyi.ctr <irybitskyi.ctr@ten-x.com>
2016-07-19 23:06:11 -0700

    add APPLICATION_FORM_URLENCODED_VALUE

--- src/main/java/pdp/api/rest/POFController.java
+++ src/main/java/pdp/api/rest/POFController.java
@@ -2,13 +2,13 @@ package pdp.api.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class POFController {
@@ -20,10 +20,10 @@ public class POFController {
 		return "PDP Slack API";
 	}
 
-	@RequestMapping(value = {"/pof"}, method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.OK)
-	public void pofAction(@RequestBody Object body) {
-		LOGGER.error("pofAction:" + body);
-//		return "ok";
+	@RequestMapping(value = {"/pof"},  method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE )
+	@ResponseBody
+	public String pofAction(@RequestBody final MultiValueMap<String, String > formVars) {
+		LOGGER.error("pofAction:" + formVars);
+		return "ok";
 	}
 }
