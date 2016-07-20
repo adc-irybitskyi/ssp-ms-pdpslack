3c4de021b058e757b3b46a6d65b3094d6af033da
irybitskyi.ctr <irybitskyi.ctr@ten-x.com>
2016-07-19 22:51:26 -0700

    change to void

--- src/main/java/pdp/api/rest/POFController.java
+++ src/main/java/pdp/api/rest/POFController.java
@@ -2,11 +2,13 @@ package pdp.api.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class POFController {
@@ -19,9 +21,9 @@ public class POFController {
 	}
 
 	@RequestMapping(value = {"/pof"}, method = RequestMethod.POST)
-	@ResponseBody
-	public String pofAction(@RequestBody Object body) {
+	@ResponseStatus(HttpStatus.OK)
+	public void pofAction(@RequestBody Object body) {
 		LOGGER.error("pofAction:" + body);
-		return "ok";
+//		return "ok";
 	}
 }
